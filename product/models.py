5358d0819fb461a8038b22bdd8de996b3090cde7
pushpa sthapit <pushpasthapit@gmail.com>
2015-09-15 10:47:33 +0545

    adsasd

--- product/models.py
+++ product/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+	Product_type = models.CharField(max_length = 100)
+	Product_name = models.CharField(max_length = 100)
+	Product_code = models.CharField(max_length = 100)
+	price = models.IntegerField()
+	created_date = models.TimeField(auto_now = True)
+	updated_date = models.TimeField(auto_now = True)
+
+
+
--- product/views.py
+++ product/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, render_to_response
+from .models import Product
 # Create your views here.
+def show(request):
+	products = Product.objects.all()
+	return render_to_response('show.html',{'products':products'})
+    
+
